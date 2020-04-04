657a3004fd5b46aa11727e8553a1441584ad2710
thiagoneves_navita <ttn.passos@gmail.com>
2020-04-04 16:24:13 -0300

    - Using Handle with external class and listener and service (like listener) ;-)

--- app/src/main/java/com/thiagoneves/myapplication/MainActivity.java
+++ app/src/main/java/com/thiagoneves/myapplication/MainActivity.java
@@ -2,11 +2,16 @@ package com.thiagoneves.myapplication;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
+import android.os.Message;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.lang.ref.WeakReference;
+
+import static com.thiagoneves.myapplication.ProcessThread.CODE_MESSAGE_FINISHED;
+import static com.thiagoneves.myapplication.ProcessThread.CODE_MESSAGE_RUNNING;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,19 +23,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.text_view);
 
-        new Thread() { //this thread run in background
+        ProcessThread processThread = new ProcessThread(new IncomingHandler(new UDPListenerService() {
             @Override
-            public void run() {
-                for (int i = 0; i <= 10; i++) {
-                    final int value = i;
-                    try {
-                        Thread.sleep(1000);
-                        Toast.makeText(MainActivity.this, "Error try show in main thread", Toast.LENGTH_LONG).show();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
+            public void handleMessage(Message msg) {
+                updateUI(msg);
             }
-        }.start();
+        }));
+        processThread.start();
+    }
+
+    static class IncomingHandler extends Handler {
+        private final WeakReference<UDPListenerService> mService;
+
+        IncomingHandler(UDPListenerService service) {
+            mService = new WeakReference<>(service);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            UDPListenerService service = mService.get();
+            if (mService.get() != null) {
+                service.handleMessage(msg);
+            }
+        }
+    }
+
+    private void updateUI(Message msg) {
+
+        if (msg.what == CODE_MESSAGE_RUNNING) {
+            //Convert object to string, typeOf result expected
+            String text = (String) msg.obj;
+            //set text in textview
+            textView.setText(text);
+
+        } else if (msg.what == CODE_MESSAGE_FINISHED) { //finish processing
+
+            long value = (long) msg.obj;
+            textView.setText("Finish processing, the random value is " + value);
+
+            Toast.makeText(this, "Android Handler - Finish", Toast.LENGTH_LONG).show();
+        }
     }
 }
+
+
---
+++ app/src/main/java/com/thiagoneves/myapplication/UDPListenerService.java
@@ -0,0 +1,7 @@
+package com.thiagoneves.myapplication;
+
+import android.os.Message;
+
+interface UDPListenerService {
+    void handleMessage(Message message);
+}
